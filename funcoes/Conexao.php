c9052230f4be8dbf577077404204bfba2126012d
sisteminformatica <63170091+sisteminformatica@users.noreply.github.com>
2022-10-08 10:31:44 -0300

    atua 081022
    atua sistemsite

---
+++ funcoes/Conexao.php
@@ -0,0 +1,10 @@
+<?php
+$servidor = 'localhost';
+$usuario  = 'meupedid_portal';
+$senha 	  = 'GWiepWPGyr3m30pt';
+$banco    = 'meupedid_portal';
+
+// FUNCOES DO SISTEMA DE CADASTRO ###########
+
+$nomesite  = 'MEUPEDIDO.DIGITAL';
+$urlmaster = 'http://meupedido.digital'; // APENAS A URL PRINCIPAL SEM A BARRA NO FINAL ---- ----
